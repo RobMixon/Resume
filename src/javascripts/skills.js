f74013cd2bf2e45c42fadb8ac5698dbdeaea6b70
RobMixon <mixonroberth@gmail.com>
2024-04-14 21:35:04 -0600

    completed nav, footer, home, about, skills and working on Contact

---
+++ src/javascripts/skills.js
@@ -0,0 +1,35 @@
+const softSkills = [
+    'Problem-Solving', 'Communication', 'Self-motivated', 'Adaptability', 'Project management',
+    'Documentation', 'Time Management', 'Willingness to learn', 'Teamwork', 'Driven'
+];
+
+const hardSkills = [
+    'Testing', 'Interaction Design', 'Version Control', 'Design Thinking',
+    'Databases', 'Decision Mapping', 'Prototyping'
+];
+
+const uiUxDesign = [
+    'Figma', 'Sketch', 'Canva'
+];
+
+const programmingDevelopment = [
+    'HTML', 'CSS', 'GIT', 'TypeScript', 'React', 'AngularJS', 'NodeJS', 'NESTJS',
+     'C#', '.Net', 'Python', 'C++', 
+     'MongoDB', 'SQL'
+];
+
+
+function createSkillList(skillsArray, containerId, title) {
+    const container = document.getElementById(containerId);
+    let html = `<h3>${title}</h3><ul>`;
+    skillsArray.forEach(skill => {
+        html += `<li>${skill}</li>`;
+    });
+    html += '</ul>';
+    container.innerHTML = html;
+}
+
+createSkillList(softSkills, 'soft-skills-column', 'Soft Skills');
+createSkillList(hardSkills, 'hard-skills-column', 'Hard Skills');
+createSkillList(uiUxDesign, 'ui-ux-design-column', 'UI/UX Design');
+createSkillList(programmingDevelopment, 'programming-development-column', 'Programming & Development');
